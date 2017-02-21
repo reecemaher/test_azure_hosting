b182df60ef02913df0a826093847cea3b54dc014
reecemaher <K00198812@student.lit.ie>
2017-02-21 15:51:44 +0000

    form and basic list

--- test_task3/routes/index.js
+++ test_task3/routes/index.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/jokes',function(req,res,next){
+  var myJoke = req.body.joke;
+
+  res.render('jokes',{jokeList:myJoke})
+  
+});
+
+
+
 module.exports = router;
