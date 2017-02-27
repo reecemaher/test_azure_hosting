2091550ecb0f36586b89ede4aa972cf46cfbfedf
reecemaher <K00198812@student.lit.ie>
2017-02-27 19:30:49 +0000

    array and loop working

--- test_task3/routes/index.js
+++ test_task3/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var myJokes =[];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('jokes', { title: 'Jokes' });
+router.get('/jokes', function(req, res, next) {
+  res.render('jokes', { title: 'Jokes',lists: myJokes });
 /*
   console.log("req.session.myJokes:" + req.session.myJokes);
 
@@ -12,12 +13,13 @@ router.get('/', function(req, res, next) {
     console.log("new Session", myJokes);
     
   }*/
+    
 });
 
 router.post('/jokes',function(req,res,next){
   var myJoke = req.body.joke;
-
-  res.render('jokes',{jokeList:myJoke});
+  myJokes.push(myJoke);
+  res.render('jokes', {lists: myJokes});
   
 });
 
--- test_task3/routes/users.js
+++ test_task3/routes/users.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('index', { title: 'Jokes' });
+});
+
+router.post('/users',function(req,res,next){
+ // var myJoke = req.body.joke;
+  var myJokes =["hi","well"];
+  //myJokes.push(myJoke);
+
+  res.render('users',{myJokes});
+  
 });
 
 module.exports = router;
